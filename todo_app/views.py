bef1a2129c79bf66f3693d5d1ba3a3d681cc1204
Ka7iposha <mixail.sakulin@bk.ru>
2023-04-11 12:13:28 +0300

    Функционал

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Todolist, ToDoItem
+from django.urls import reverse
 
 
 class ListListView(ListView):
@@ -19,3 +20,51 @@ class ItemListView(ListView):
         context["todo_list"] = Todolist.objects.get(id=self.kwargs["list_id"])
         return context
 
+
+class ListCreate(CreateView):
+    model = Todolist
+    fields = ["title"]
+    template_name = "todo_app/todo_list_form.html"
+
+    def get_context_data(self):
+        context = super(ListCreate, self).get_context_data()
+        context["title"] = "Добавить новый список"
+        return context
+
+
+class ItemCreate(CreateView):
+    model = ToDoItem
+    fields = ["todo_list", "category", "title", "description", "due_date", "complete"]
+    template_name = "todo_app/todo_item_form.html"
+
+    def get_initial(self):
+        initial_data = super(ItemCreate, self).get_initial()
+        todo_list = Todolist.objects.get(id=self.kwargs["list_id"])
+        initial_data["todo_list"] = todo_list
+        return initial_data
+
+    def get_context_data(self):
+        context = super(ItemCreate, self).get_context_data()
+        todo_list = Todolist.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = todo_list
+        context["title"] = "Создать новую задачу"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+
+
+class ItemUpdate(UpdateView):
+    model = ToDoItem
+    fields = ["todo_list", "category", "title", "description", "due_date", "complete"]
+    template_name = "todo_app/todo_item_form.html"
+
+
+    def get_context_data(self):
+        context = super(ItemUpdate, self).get_context_data()
+        context["todo_list"] = self.object.todo_list
+        context["title"] = "Редактировать задачу"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
